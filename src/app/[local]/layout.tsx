0261c8e69f81f96f5668e682520e25b9c85b471a
samer <judosamer555@gmail.com>
2026-03-06 10:11:25 +0200

    fixed the params type

--- src/app/[local]/layout.tsx
+++ src/app/[local]/layout.tsx
@@ -2,13 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import RootProviders from "@/components/providers";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-
-type Props = {
-  children: React.ReactNode;
-  params: {
-    locale: Locale;
-  };
-};
+import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +15,20 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// Generate static params for locales for build time
+// Generate static params for locals for build time
 export function generateStaticParams() {
-  return [{ locale: "en" }, { locale: "ar" }];
+  return [{ local: "en" }, { local: "ar" }];
 }
 
-export default async function RootLayout({ children, params }: Props) {
+export default async function RootLayout({ children, params }: { children: React.ReactNode; params: Promise<{ local: string }> }) {
+  const { local } = await params;
+
+  // Check if the locale is valid
+  if (!routing.locales.includes(local as Locale)) {
+    notFound();
+  }
   return (
-    <html lang={params.locale} dir={params.locale === "ar" ? "rtl" : "ltr"} suppressHydrationWarning>
+    <html lang={local} dir={local === "ar" ? "rtl" : "ltr"} suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}>
         <ErrorBoundary fallback={<div>Something went wrong!</div>}>
           <Suspense fallback={<div>Loading...</div>}>
